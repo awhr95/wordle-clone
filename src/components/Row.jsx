5c019bbe25e192e7e99b70813a8cdf1773228941
awhr95 <awhr95@gmail.com>
2024-04-20 14:33:36 +0100

    Add logic for letters correct/absent

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "../styles/Row.scss";
 
-export default function Wordle({ word }) {
+export default function Wordle({
+  word,
+  markAsSolution,
+  markPresentAndAbsentLetters,
+  solution,
+  bounceOnError,
+}) {
   return (
-    <div className="row">
+    <div className={`row ${bounceOnError && "row--bounce"}`}>
       {word.split("").map((letter, index) => {
+        const bgClass = solution.includes(letter) ? "present" : "absent";
+
         return (
-          <div className="letter" key={index}>
+          <div
+            className={`letter ${
+              markAsSolution && `correct rotate--${index + 1}00`
+            } ${
+              markPresentAndAbsentLetters && `${bgClass} rotate--${index + 1}00`
+            } ${letter !== " " && "letter--active"} ${
+              solution[index] === letter && "correct"
+            }`}
+            key={index}
+          >
             {letter}
             <div className="back">{letter}</div>
           </div>
